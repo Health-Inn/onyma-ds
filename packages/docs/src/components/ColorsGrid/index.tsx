5ea369f87f9048e15d33efe5a5bcfaeea0fa71b3
Gabriel Belther <belthergabriel@gmail.com>
2022-12-13 18:56:41 -0300

    feat: create colors tokens

--- packages/docs/src/components/ColorsGrid/index.tsx
+++ packages/docs/src/components/ColorsGrid/index.tsx
@@ -1,26 +1,23 @@
 import { colors } from "@onyma-ds/tokens";
-import { getContrast } from "polished";
+import { replaceUnderlineBy, titleCase } from "../../utils/stringFormat";
 
 import "./styles.css";
 
 export const ColorsGrid = () => {
-  const calculateContrast = (
-    color: string,
-    colorToCompare: string = "#fff"
-  ) => {
-    const contrast = getContrast(color, colorToCompare);
-    return contrast < 3.5;
-  };
-
   return (
     <div className="cg-container">
       {Object.entries(colors).map(([colorName, colorHex]) => (
-        <div
-          className="cg-container__item"
-          style={{ backgroundColor: colorHex }}
-        >
-          <p className={`${calculateContrast(colorHex) ? "dark" : "light"}`}>
-            ${colorName}
+        <div className="cg-container__item">
+          <div
+            className="cg-container__item--color-box"
+            style={{ backgroundColor: colorHex }}
+          />
+          <p className="cg-container__item--variable-name">${colorName}</p>
+          <p className="cg-container__item--color-name">
+            {titleCase(replaceUnderlineBy(colorName))}
+          </p>
+          <p className="cg-container__item--color-hex">
+            {colorHex.toLocaleUpperCase()}
           </p>
         </div>
       ))}
