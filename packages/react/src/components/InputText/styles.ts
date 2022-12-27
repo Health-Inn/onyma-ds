21005fcac4acdbe5b094de4b971d01d0e4cbabfb
Gabriel Belther <belthergabriel@gmail.com>
2022-12-27 09:26:19 -0300

    feat: altura do campo

--- packages/react/src/components/InputText/styles.ts
+++ packages/react/src/components/InputText/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { theme } from "@onyma-ds/tokens";
 
 export const Container = styled.input`
+  box-sizing: border-box;
   font-size: ${theme.typographies.body_03.fontSize};
   font-family: ${theme.typographies.body_03.fontFamily};
   font-weight: ${theme.typographies.body_03.fontWeight};
@@ -9,6 +10,7 @@ export const Container = styled.input`
   background-color: ${theme.colors.white};
   border: 1px solid ${theme.colors.gray_80};
   width: 100%;
+  height: 2.75rem;
   border-radius: 4px;
   padding: 0.75rem;
   outline: none;
